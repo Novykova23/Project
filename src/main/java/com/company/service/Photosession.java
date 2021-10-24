81263ad7c2504201e3341530326f24c534ad7914
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-24 20:53:34 +0300

    add photosession

--- src/main/java/com/company/service/Photosession.java
+++ src/main/java/com/company/service/Photosession.java
@@ -1,2 +1,50 @@
-package com.company.service;public class Photosession {
+package com.company.service;
+
+import com.company.money.Money;
+import com.company.person.Customer;
+import com.company.person.Photographer;
+
+public final class PhotoSession{
+    private final int amountOfPhotos;
+    private final int customerList;
+    private final Money cost;
+    private final Photographer photographer;
+    private Money Objects;
+
+    public PhotoSession(int amountOfPhotos, int customerList, Money cost, Photographer photographer) {
+        assert amountOfPhotos > 0 : "the photographer should take at least one photo";
+        assert cost.amount() != 0 : "customers should pay for photos";
+
+        this.amountOfPhotos = amountOfPhotos;
+        this.customerList = customerList;
+        this.cost = cost;
+        this.photographer = photographer;
+    }
+
+    public int getAmountOfPhotos() {
+        return amountOfPhotos;
+    }
+
+    public int getCustomerList() {
+        return customerList;
+    }
+
+    public Money getCost() {
+        return cost;
+    }
+
+    public Photographer getPhotographer() {
+        return photographer;
+    }
+
+
+    @Override
+    public String toString() {
+        return "PhotoSession{" +
+                ", amountOfPhotos=" + amountOfPhotos +
+                ", customerList=" + customerList +
+                ", cost=" + cost +
+                ", photographer=" + photographer +
+                '}';
+    }
 }
