9faa32456b128ead1c03b030c77c907a3ad29599
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-24 21:00:27 +0300

    add currency

--- src/main/java/com/company/money/Currency.java
+++ src/main/java/com/company/money/Currency.java
@@ -1,2 +1,21 @@
-package com.company.money;public class Currency {
+package com.company.money;
+public enum Currency {
+    USD("Dollar", "Dollars"),
+    UAH("Hryvna", "Hryvnias"),
+    EUR("Euro", "Euro");
+    private final String text;
+    private final String nText;
+
+    Currency(String text, String nText) {
+        this.text = text;
+        this.nText = nText;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getnText() {
+        return nText;
+    }
 }
