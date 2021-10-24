1eb9e05fb39260932ebe2cecd466f5dbfbe9803b
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-24 20:59:29 +0300

    add money

--- src/main/java/com/company/money/Money.java
+++ src/main/java/com/company/money/Money.java
@@ -1,2 +1,65 @@
-package com.company.money;public class Money {
+package com.company.money;
+
+import com.company.person.Owner;
+
+public final class Money {
+    private final int amount;
+    private final Currency currency;
+    private Owner Objects;
+
+    public Money(int amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public int amount() {
+        return amount;
+    }
+
+    public Currency currency() {
+        return currency;
+    }
+
+    public static Money usd(int amount) {
+        return new Money(amount, Currency.USD);
+    }
+
+    public static Money eur(int amount) {
+        return new Money(amount, Currency.EUR);
+    }
+
+    public static Money uah(int amount) {
+        return new Money(amount, Currency.UAH);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Money) obj;
+        return this.amount == that.amount &&
+                Objects.equals(this.currency, that.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
+
+    @Override
+    public String toString() {
+        final String currencyLabel = amount == 1 ? currency.getnText() : currency.getText();
+
+        return String.format("%d %s", amount, currencyLabel);
+    }
+
 }
+
