72b54659d60c62f237fe3a8ec75852f8b89e70c8
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-24 20:48:20 +0300

    add photographer

--- src/main/java/com/company/person/Photographer.java
+++ src/main/java/com/company/person/Photographer.java
@@ -1,2 +1,39 @@
-package com.company.person;public class Photographer {
+package com.company.person;
+
+import com.company.money.Currency;
+
+public final class Photographer extends Person {
+    public Photographer(String givenName, String familyName) {
+        super(givenName, familyName);
+    }
+
+    @Override
+    public String getGivenName() {
+        return super.getGivenName();
+    }
+
+    @Override
+    public String getFamilyName() {
+        return super.getFamilyName();
+    }
+
+    @Override
+    public String getDateOfBirth() {
+        return super.getDateOfBirth();
+    }
+
+    @Override
+    public String getId() {
+        return super.getId();
+    }
+
+    @Override
+    public int hash(int amount, Currency currency) {
+        return 0;
+    }
+
+    @Override
+    public long getAge() {
+        return super.getAge();
+    }
 }
