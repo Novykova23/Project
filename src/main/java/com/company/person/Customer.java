f17fda76354f7bed0c291d81e669c49286e64515
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-24 20:45:06 +0300

    add customer

--- src/main/java/com/company/person/Customer.java
+++ src/main/java/com/company/person/Customer.java
@@ -1,2 +1,31 @@
-package com.company.person;public class Customer {
+package com.company.person;
+
+import com.company.money.Currency;
+
+public final class Customer extends Person {
+    public Customer(String givenName, String familyName) {
+        super(givenName);
+    }
+
+    @Override
+    public String getGivenName() {
+        return super.getGivenName();
+    }
+
+    @Override
+    public String getFamilyName() {
+        return super.getFamilyName();
+    }
+
+
+    @Override
+    public int hash(int amount, Currency currency) {
+        return 0;
+    }
+
+    @Override
+    public long getAge() {
+        return super.getAge();
+    }
 }
+
