27ee270ffa5cec3dd9caa8f5929c531d6bd69758
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-28 16:55:13 +0300

    add money

--- src/main/java/com/company/money/Money.java
+++ src/main/java/com/company/money/Money.java
@@ -5,7 +5,6 @@ import com.company.person.Owner;
 public final class Money {
     private final int amount;
     private final Currency currency;
-    private Owner Objects;
 
     public Money(int amount, Currency currency) {
         this.amount = amount;
@@ -40,19 +39,6 @@ public final class Money {
         return new Money(amount, Currency.UAH);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Money) obj;
-        return this.amount == that.amount &&
-                Objects.equals(this.currency, that.currency);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount, currency);
-    }
 
     @Override
     public String toString() {
