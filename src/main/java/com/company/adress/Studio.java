6df772563689d097f40a646357c6e70684e82d19
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-28 16:38:29 +0300

    add studio

--- src/main/java/com/company/adress/Studio.java
+++ src/main/java/com/company/adress/Studio.java
@@ -2,13 +2,10 @@ package com.company.adress;
 
 public final class Studio { ;
     private final String name;
-    private final String address;
-    private final String studioType;
-    private String owner;
-    private Adress adress;
-    private Object String;
+    private final Address address;
+    private final StudioType studioType;
 
-    public Studio(String name, Adress address, StudioType type) {
+    public Studio(String name, Address address, StudioType type) {
         this.name = name;
         this.address = address;
         this.studioType = type;
@@ -18,15 +15,12 @@ public final class Studio { ;
         return name;
     }
 
-    public Adress getAdress() {
-        return adress;
+    public Address getAddress() {
+        return address;
     }
 
-    public java.lang.String getOwner() {
-        return owner;
-    }
 
-    public String getStudioType() {
+    public StudioType getStudioType() {
         return studioType;
     }
 
