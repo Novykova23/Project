8b32b6b3741f883da5d3213e96bc732e3c9dc7c5
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-30 21:08:35 +0300

    add assistantphoto

--- src/main/java/com/company/person/AssistantPhoto.java
+++ src/main/java/com/company/person/AssistantPhoto.java
@@ -1,2 +1,12 @@
-package com.company.person;public class AssistantPhoto {
+package com.company.person;
+public class AssistantPhoto extends Person{
+
+    protected AssistantPhoto(String givenName, String familyName) {
+        super(givenName, familyName);
+    }
+
+    public interface Assistant{
+        void help();
+    }
+
 }
