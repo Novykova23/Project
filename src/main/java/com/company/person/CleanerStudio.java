6fe88b6d34a7d081dc3187e675249ea50c449051
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-10-30 21:07:16 +0300

    add cleanerstudio

--- src/main/java/com/company/person/CleanerStudio.java
+++ src/main/java/com/company/person/CleanerStudio.java
@@ -1,2 +1,10 @@
-package com.company.person;public class CleanerStudio {
+package com.company.person;
+
+public class CleanerStudio extends Person{
+    protected CleanerStudio(String givenName, String familyName) {
+        super(givenName, familyName);
+    }
+    public interface Cleaner{
+        void clean();
+    }
 }
