dba53c6b92f75371a47c11c1d088134ee6293541
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-11-03 20:06:32 +0200

    add main

--- src/main/java/com/company/main/Main.java
+++ src/main/java/com/company/main/Main.java
@@ -3,11 +3,9 @@ package com.company.main;
 import com.company.adress.Adress;
 import com.company.adress.Studio;
 import com.company.adress.StudioType;
-import com.company.money.Money;
 import com.company.person.Customer;
 import com.company.person.Owner;
 import com.company.person.Photographer;
-import com.company.service.Photosession;
 
 public class Main {
 
@@ -16,8 +14,6 @@ public class Main {
         final Studio lensQueenStudio = new Studio("Lens Queen", new Adress("Chernivtsi", "Kobilyanska. 10", "Ukraine", "58000"), StudioType.PHOTO);
         final Studio ambientPortraitureStudio = new Studio("Ambient Portraiture", new Adress("Ternopil", "Holovna, 11", "Ukraine", "58000"), StudioType.PHOTO);
 
-        owner.assignStudio(lensQueenStudio);
-        owner.assignStudio(ambientPortraitureStudio);
 
         //show owners info
         System.out.print("*************OWNER*************\n");
@@ -35,10 +31,4 @@ public class Main {
         System.out.print("*******************************\n\n");
 
     }
-
-
-    private static class Arrays{
-        public static void asList(Customer customer1, Customer customer2) {
-        }
     }
-}
