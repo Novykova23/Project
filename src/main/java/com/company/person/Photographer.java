7d3197a2e222c8966fbec81b35a3560b0615cf69
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-11-25 16:47:18 +0200

    add photographer

--- src/main/java/com/company/person/Photographer.java
+++ src/main/java/com/company/person/Photographer.java
@@ -3,6 +3,18 @@ public final class Photographer extends Person {
     private String familyName;
     private String givenName;
 
+    public Photographer(String givenName, String familyName, String familyName1, String givenName1) {
+        super(givenName, familyName);
+        this.familyName = familyName1;
+        this.givenName = givenName1;
+    }
+
+    public Photographer(String givenName, String familyName, String givenName1) {
+        super(givenName);
+        this.familyName = familyName;
+        this.givenName = givenName1;
+    }
+
     public Photographer(String givenName, String familyName) {
         super(givenName);
         this.familyName = familyName;
