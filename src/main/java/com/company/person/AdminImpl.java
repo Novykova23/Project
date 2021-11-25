25ef658b4ac9e0e3fb866857df94b9a364820735
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-11-25 16:43:05 +0200

    add admin impl

--- src/main/java/com/company/person/AdminImpl.java
+++ src/main/java/com/company/person/AdminImpl.java
@@ -6,6 +6,11 @@ public class AdminImpl extends Person implements Admin {
     protected AdminImpl(String givenName, String familyName) {
         super(givenName, familyName);
     }
+
+    public AdminImpl(String givenName) {
+        super(givenName);
+    }
+
     void acceptOrders() {
 
     }
