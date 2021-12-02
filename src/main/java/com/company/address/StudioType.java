432a496965635465588d25752be7c804558b1cd0
Novykova23 <91437756+Novykova23@users.noreply.github.com>
2021-12-02 14:00:18 +0200

    add studio type

--- src/main/java/com/company/address/StudioType.java
+++ src/main/java/com/company/address/StudioType.java
@@ -2,17 +2,4 @@ package com.company.address;
 
 public enum StudioType {
     PHOTO, VIDEO, GAME;
-
-    StudioType() {
-    }
-
-    public static <T extends Enum<T>> T valueOf(Class<T> enumType, String name) {
-        return Enum.valueOf(enumType, name);
-    }
-
-
-    @Override
-    public String toString() {
-        return "StudioType{}";
-    }
 }
